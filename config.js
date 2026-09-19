7f93d8079c32d1ae72112e92226a15f3bdf9f366
g6hdzvt6yx-del <g6hdzvt6yx@privaterelay.appleid.com>
2026-09-19 16:56:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Вставьте сюда данные из Supabase: Project Settings → API
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://dgwdotgraolyrsipzqui.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_sgVHw89bwZ1p4KJOKc_uQA_JEN_b6Rk"
+};
